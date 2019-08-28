789ae102d3f7683a7a6375a1cb15324c0de00fb6
langshiquan <576506402@qq.com>
2019-08-29 02:51:49 +0800

    init

---
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -0,0 +1,51 @@
+package io.jenkins.plugins.sample;
+
+import hudson.model.Run;
+import jenkins.model.RunAction2;
+
+import javax.annotation.CheckForNull;
+
+public class HelloWorldAction implements RunAction2 {
+    private transient Run run;
+    private String name;
+
+    public HelloWorldAction(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @CheckForNull
+    @Override
+    public String getIconFileName() {
+        return "document.png";
+    }
+
+    @CheckForNull
+    @Override
+    public String getDisplayName() {
+        return "Greeting";
+    }
+
+    @CheckForNull
+    @Override
+    public String getUrlName() {
+        return "greeting";
+    }
+
+    @Override
+    public void onAttached(Run<?, ?> run) {
+        this.run = run;
+    }
+
+    @Override
+    public void onLoad(Run<?, ?> run) {
+        this.run = run;
+    }
+
+    public Run getRun() {
+        return run;
+    }
+}
